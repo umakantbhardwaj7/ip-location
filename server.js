0c1c09e78a70c31d001c34b9410547546b50b59c
Umakant Bhardwaj <34452622+umakantbhardwaj7@users.noreply.github.com>
2024-10-11 03:04:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4005;
 
-app.use(express.static('public')); // Serve your HTML file from the 'public' folder
+app.use(express.static('public'));
 
 app.get('/get-ip', (req, res) => {
-    const ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress; // Get user's IP address
-    res.send(ip); // Send the IP address as a response
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress;
+    const realIp = ip.split(',')[0].trim();
+    res.send(realIp);
 });
 
 app.listen(PORT, () => {
